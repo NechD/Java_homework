5c97a263482ef1dbe5b7bfe00471728d9ef0f9ee
Dmitry Nechman <Nechman.D@yandex.ru>
2023-10-16 23:48:05 +0500

    LR2 Выполнена

---
+++ 2 курс/MySecondTestAppSpringBoot/src/main/java/ru/arkhipov/MySecondTestAppSpringBoot/exception/UnsupportedCodeException.java
@@ -0,0 +1,9 @@
+package ru.arkhipov.MySecondTestAppSpringBoot.exception;
+
+
+public class UnsupportedCodeException extends Exception {
+
+    public UnsupportedCodeException(String message) {
+        super(message);
+    }
+}
