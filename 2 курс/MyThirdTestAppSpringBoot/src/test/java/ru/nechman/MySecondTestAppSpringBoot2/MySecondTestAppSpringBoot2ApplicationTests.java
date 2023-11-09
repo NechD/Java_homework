41838c4183fb01d83a14d3d6960726c29cd80cce
Dmitry Nechman <Nechman.D@yandex.ru>
2023-11-10 00:11:23 +0500

    Случайные правки залетели

--- 2 курс/MyThirdTestAppSpringBoot/src/test/java/ru/nechman/MySecondTestAppSpringBoot2/MySecondTestAppSpringBoot2ApplicationTests.java
+++ 2 курс/MyThirdTestAppSpringBoot/src/test/java/ru/nechman/MySecondTestAppSpringBoot2/MySecondTestAppSpringBoot2ApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.kazakov.MySecondTestAppSpringBoot2;
+package ru.nechman.MySecondTestAppSpringBoot2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
